015a9009d22d1506a59cb24b41b57efc48065829
Janhvi Shukla <92948405+janhvi-shukla@users.noreply.github.com>
2023-12-05 23:31:50 +0530

    replace.py

---
+++ Day_9/replace.py
@@ -0,0 +1,12 @@
+a="hy janhvi there"
+b=a.lower()
+print(b)
+c=a.count("a")
+print("The count of a is ",c)
+for i in range(len(a)):
+    if(a[i]=="a"):
+        print(i)
+d=a.replace("a","iNeuron")
+print(d)
+e=a.split()
+print(e)
