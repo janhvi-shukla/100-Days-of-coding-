263f8587864a454f041f611263afe9088eaeb369
Janhvi Shukla <92948405+janhvi-shukla@users.noreply.github.com>
2023-12-30 23:38:59 +0530

    convertion.py

---
+++ Day_33/convertion.py
@@ -0,0 +1,6 @@
+Create a tuple containing individual characters
+tup = ('e', 'x', 'e', 'r', 'c', 'i', 's', 'e', 's')
+# Use the 'join' method to concatenate the characters in the tuple without any spaces and create a single string
+str = ''.join(tup)
+# Print the resulting string
+print(str)
