b71ead419aee0281db72e22761d72b78aece6884
Janhvi Shukla <92948405+janhvi-shukla@users.noreply.github.com>
2023-12-31 23:30:40 +0530

    dict.py

---
+++ Day_34/dict.py
@@ -0,0 +1,24 @@
+# create a dictionary using {}
+person = {"name": "Jessa", "country": "USA", "telephone": 1178}
+print(person)
+# output {'name': 'Jessa', 'country': 'USA', 'telephone': 1178}
+
+# create a dictionary using dict()
+person = dict({"name": "Jessa", "country": "USA", "telephone": 1178})
+print(person)
+# output {'name': 'Jessa', 'country': 'USA', 'telephone': 1178}
+
+# create a dictionary from sequence having each item as a pair
+person = dict([("name", "Mark"), ("country", "USA"), ("telephone", 1178)])
+print(person)
+
+# create dictionary with mixed keys keys
+# first key is string and second is an integer
+sample_dict = {"name": "Jessa", 10: "Mobile"}
+print(sample_dict)
+# output {'name': 'Jessa', 10: 'Mobile'}
+
+# create dictionary with value as a list
+person = {"name": "Jessa", "telephones": [1178, 2563, 4569]}
+print(person)
+# output {'name': 'Jessa', 'telephones': [1178, 2563, 4569]}
